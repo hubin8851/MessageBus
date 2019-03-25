d2d4d41ce705436344718e864bd4c93bee32dac5
hubinxing <376898978@qq.com>
2019-03-25 11:37:25 +0800

    添加项目文件。

---
+++ MessageBus/BaseMesBus.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <functional>
+#include <map>
+#include <tuple>
+#include <type_traits>
+
+#include "Func_traits.h"
+
+//消息总线类
+class BaseMes_bus
+{
+private:
+	//调用器
+	std::map<std::string, std::function<void(void*, void*)>> invokers_map;
+protected:
+public:
+	//注册的回调函数
+	template<typename Function>
+	void Register_handler(std::string const & name, const Function& f)
+	{
+		using std::placeholders::_1;
+		using std::placeholders::_2;
+
+		using return_type = typename timax::function_traits<Function>::result_type;
+
+		this->invokers_map[name] = { std::bind(&invoker<Function>::apply, f, _1, _2) };
+	}
+
+
+
+
+};
